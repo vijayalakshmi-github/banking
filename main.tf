e64e2442eecc960c347caf72f4fd2632b0277802
vijayalakshmi-github <vichusuguntan3@gmail.com>
2023-04-24 15:45:37 +0000

    c

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "prod-server"{
 	ami = "ami-007855ac798b5175e"
 	instance_type = "t2.micro"
 	availability_zone = "us-east-1a"
-	key_name = aws
+	key_name = "aws"
 	vpc_security_group_ids =["sg-0d665c5855066777e"]
 	tags = {
 	  Name = "prod-server"
