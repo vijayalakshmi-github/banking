1ab3d7da98f29e8423156010f1c1b6ce446fa193
vijayalakshmi-github <vichusuguntan3@gmail.com>
2023-04-24 19:35:20 +0000

    c

--- main.tf
+++ main.tf
@@ -3,23 +3,22 @@ provider "aws"{
         secret_key = "M7Fk9ya5skAkXW3MYfWPbk0ViVJhX31oCotA6vVT"
         region = "us-east-1"
 }
-resource "aws_instance" "prod-server"{
+resource "aws_instance" "prod-server1"{
 	ami = "ami-007855ac798b5175e"
 	instance_type = "t2.micro"
-	availability_zone = "us-east-1a"
 	key_name = "aws"
 	vpc_security_group_ids =["sg-0d665c5855066777e"]
-	tags = {
-	  Name = "prod-server"
-	}
 	connection{
 		type = "ssh"
 		user = "ubuntu"
 		private_key = file("./aws.pem")
 		host = self.public_ip
 	}
+	tags = {
+	  Name = "prod-server1"
+	}
 	provisioner "local-exec"{
-		command = "echo ${aws_instance.prod-server.public_ip} > inventory"
+		command = "echo ${aws_instance.prod-server1.public_ip} > inventory"
 	}
 	provisioner "local-exec"{
 		command = "ansible-playbook /var/lib/jenkins/workspace/banking/deploy.yml"
