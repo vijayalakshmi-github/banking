a011f34c5f039dc32138734db621c87643b3df9a
vijayalakshmi-github <vichusuguntan3@gmail.com>
2023-04-24 19:40:07 +0000

    c

--- main.tf
+++ main.tf
@@ -7,20 +7,4 @@ resource "aws_instance" "prod-server1"{
 	ami = "ami-007855ac798b5175e"
 	instance_type = "t2.micro"
 	key_name = "aws"
-	vpc_security_group_ids =["sg-0d665c5855066777e"]
-	connection{
-		type = "ssh"
-		user = "ubuntu"
-		private_key = file("./aws.pem")
-		host = self.public_ip
-	}
-	tags = {
-	  Name = "prod-server1"
-	}
-	provisioner "local-exec"{
-		command = "echo ${aws_instance.prod-server1.public_ip} > inventory"
-	}
-	provisioner "local-exec"{
-		command = "ansible-playbook /var/lib/jenkins/workspace/banking/deploy.yml"
-	}
 }
