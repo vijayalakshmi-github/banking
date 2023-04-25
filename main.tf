7a64b8e93f5bb35588511c2ebfece704fba2bcf8
vijayalakshmi-github <vichusuguntan3@gmail.com>
2023-04-25 07:52:19 +0000

    c

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "prod-server1"{
    	  Name = "prod-server1"
 	}
 	connection {
+		command="echo establishing connection.."
 		  type        = "ssh"
 		  user        = "ubuntu"
 		  private_key = file("aws.pem")
