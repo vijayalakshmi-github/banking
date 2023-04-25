b589616118b20f3bea6450d463d7f59b1c0f48ed
vijayalakshmi-github <vichusuguntan3@gmail.com>
2023-04-25 08:05:49 +0000

    c

--- main.tf
+++ main.tf
@@ -9,16 +9,10 @@ resource "aws_instance" "prod-server1"{
 	tags = {
    	  Name = "prod-server1"
 	}
-	connection {
-		  type        = "ssh"
-		  user        = "ubuntu"
-		  private_key = file("aws.pem")
-		  host        = aws_instance.prod-server1.public_ip
-	}	
         provisioner "local-exec"{
 		command = "echo ${aws_instance.prod-server1.public_ip} > inventory"
 	}
 	provisioner "local-exec"{
-		command = "ansible-playbook -i /var/lib/jenkins/workspace/banking/inventory /var/lib/jenkins/workspace/banking/deploy.yml"
+		command = "ansible-playbook /var/lib/jenkins/workspace/banking/deploy.yml"
 	}
 }
